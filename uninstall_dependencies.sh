cb38e0514021f0794cc829e21df4d282d55fa168
yago <yago.casasnovas@gmail.com>
2017-08-15 12:17:35 +0200

    s

---
+++ uninstall_dependencies.sh
@@ -0,0 +1,14 @@
+
+apt-get purge nodejs --assume-yes
+
+apt-get purge nodejs nodejs-legacy --assume-yes
+
+apt-get purge gnome-terminal --assume-yes
+
+apt-get purge npm --assume-yes
+
+apt-get purge redis-server --assume-yes
+
+apt autoremove --assume-yes
+
+rm /home/yago/Escritorio/islarobot
