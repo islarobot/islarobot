2534c8a1e0b1c4f947dc2ff077c6e582fb4503fd
yago <islarobots@gmail.com>
2017-09-01 13:49:57 +0200

    s

--- uninstall_dependencies.sh
+++ uninstall_dependencies.sh
@@ -9,9 +9,7 @@ apt-get purge npm --assume-yes
 
 apt-get purge redis-server --assume-yes
 
-apt-get purge python-pip --assume-yes
-
-pip uninstall ino
+rm /usr/local/bin/arduino
 
 apt-get purge libcairo2-dev libjpeg-dev libpango1.0-dev libgif-dev build-essential --assume-yes
 
